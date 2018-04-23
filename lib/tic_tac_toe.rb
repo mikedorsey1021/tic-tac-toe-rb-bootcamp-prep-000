80553f8273b7b3dab03722d5ba26fdca133f63d1
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-23 15:15:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -180,9 +180,12 @@ def turn_count(board)
   counter
 end
 
-def current_player(board)
-  turn_count(board).even? ? player = "X" : player = "O"
-  player
+def current_player (board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
 end
 
 WIN_COMBINATIONS = [
